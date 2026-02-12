d4f74906ad33fde4d81ffba6f5d9973b5b05d3ba
keshavsaini07 <sainikeshav770@gmail.com>
2026-02-12 13:20:05 +0530

    feat: added frontend setup

--- apps/api/src/routes/tx.routes.ts
+++ apps/api/src/routes/tx.routes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from "fastify";
 import * as txController from "../controllers/tx.controller";
 
 export default async function txRoutes(fastify: FastifyInstance) {
-  fastify.post("/tx/encrypt", txController.encrypt);
+  fastify.post("/encrypt", txController.encrypt);
 }
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="bg-amber-100">
+      <h1 className="font-sans font-semibold text-xl">Secure Transaction Mini App</h1>
+    </div>
+  );
+}
